635c7f0c5cfa687f95c0ce1134974290fbed177d
Christopher M. Judd <javajudd@gmail.com>
2011-08-11 22:13:41 -0400

    Added base as well as ipad and iphone specific view controllers to list notes.

---
+++ EveryNote/NoteListViewController_iPad.h
@@ -0,0 +1,16 @@
+//
+//  NoteListViewController_iPad.h
+//  EveryNote
+//
+//  Created by Christopher Judd on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NoteListViewController_iPad : UITableViewController {
+    
+}
+
+@end
---
+++ EveryNote/NoteListViewController_iPhone.m
@@ -0,0 +1,15 @@
+//
+//  NoteListViewController_iPhone.m
+//  EveryNote
+//
+//  Created by Christopher Judd on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NoteListViewController_iPhone.h"
+
+
+@implementation NoteListViewController_iPhone
+
+
+@end
